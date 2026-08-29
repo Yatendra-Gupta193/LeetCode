1357b41e3fff4e60eaa6a994344baf0138a26a09
Yatendra Kumar Gupta <guptajikrish12@gmail.com>
2026-08-29 10:38:58 +0530

    Time: 14 ms (92.16%), Space: 51.2 MB (87.58%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int childIdx=0 , cookieIdx=0; 
+        while(childIdx<g.length && cookieIdx<s.length){
+            if(s[cookieIdx] >= g[childIdx]){
+                childIdx++;
+            }
+            cookieIdx++;
+        }
+        return childIdx;
+    }
+}
